2d17a47d0fd07a0dcd0508ab41d67854b1eb98de
aisfarhan415 <aisfarhan.professional@gmail.com>
2026-05-31 23:54:48 +0700

    Initial Commit

---
+++ BatteryHub/BatteryHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryHubApp.swift
+//  BatteryHub
+//
+//  Created by Ais Farhan on 5/31/26.
+//
+
+import SwiftUI
+
+@main
+struct BatteryHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
